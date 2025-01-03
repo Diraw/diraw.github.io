b51c145e230a71f6bfe7ceff6f328b6da85bbc2d
diraw <craftyup7567@outlook.com>
2025-01-03 17:58:19 +0800

    Site updated: 2025-01-03 17:58:18

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
   if (window.Pjax) {
-    const isFirstVisit = !sessionStorage.getItem('firstVisit');
-
-    if (isFirstVisit) {
-      document.body.classList.add('pjax-enter-active');
-      setTimeout(function () {
-        document.body.classList.remove('pjax-enter-active');
-      }, 1000); // 与 CSS transition 时间一致
-      sessionStorage.setItem('firstVisit', 'true');
-    }
-
     new Pjax({
       elements: 'a',
       selectors: ['title', '.content'],
@@ -22,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.addEventListener('pjax:complete', function () {
       document.body.classList.remove('pjax-leave-active');
+      document.body.classList.add('pjax-enter-active');
+      setTimeout(function () {
+        document.body.classList.remove('pjax-enter-active');
+      }, 500);
     });
   }
 });
