2d888fe7a53e2a32f87db86b52848dafc04893f6
diraw <craftyup7567@outlook.com>
2025-01-05 23:56:50 +0800

    Site updated: 2025-01-05 23:56:49

--- js/custom.js
+++ js/custom.js
@@ -28,33 +28,31 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function checkBackgroundImage() {
-  document.body.style.visibility = 'hidden';
-  document.body.style.overflow = 'hidden';
   var fullPageElement = document.querySelector('.full_page, .post-bg');
 
   if (fullPageElement) {
-    console.log('Full-page element detected.');
+    // console.log('Full-page element detected.');
 
     var style = window.getComputedStyle(fullPageElement);
     var backgroundImage = style.backgroundImage;
-    console.log('Background image:', backgroundImage);
+    // console.log('Background image:', backgroundImage);
 
     // 提取 URL，去除前5个字符和后2个字符
     var imageUrl = backgroundImage.slice(5, -2);
-    console.log('Background image URL:', imageUrl);
+    // console.log('Background image URL:', imageUrl);
 
     if (imageUrl && imageUrl !== 'none') {
       var img = new Image();
       img.crossOrigin = "anonymous"; // 如果有跨域问题，尝试添加这行
 
       img.onload = function() {
-        console.log('Background image loaded.');
+        // console.log('Background image loaded.');
         document.body.style.visibility = 'visible';
         document.body.style.overflow = 'auto';
       };
 
       img.onerror = function() {
-        console.error('Failed to load background image.');
+        // console.error('Failed to load background image.');
         document.body.style.visibility = 'visible';
         document.body.style.overflow = 'auto';
       };
@@ -62,12 +60,12 @@ function checkBackgroundImage() {
       // 确保图片路径是正确的
       img.src = imageUrl;
     } else {
-      console.log('No background image found.');
+      // console.log('No background image found.');
       document.body.style.visibility = 'visible';
       document.body.style.overflow = 'auto';
     }
   } else {
-    console.log('No full-page element detected.');
+    // console.log('No full-page element detected.');
     document.body.style.visibility = 'visible';
     document.body.style.overflow = 'auto';
   }
