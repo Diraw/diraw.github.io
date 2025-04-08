bfd9413b65cad49c2f12c767deaba7742f048c01
diraw <craftyup7567@outlook.com>
2025-04-08 16:07:08 +0800

    Site updated: 2025-04-08 16:07:07

--- js/custom.js
+++ js/custom.js
@@ -87,19 +87,22 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', upd
 
 // 将导航的搜索功能移动到最后去
 document.addEventListener('DOMContentLoaded', function() {
-  var searchButton = document.getElementById('search-button');
-  var menusItems = document.querySelectorAll('.menus_items');
+  // 检查屏幕宽度是否大于某个值（例如768px，通常认为是平板/手机与桌面设备的分界点）
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    var searchButton = document.getElementById('search-button');
+    var menusItems = document.querySelectorAll('.menus_items');
 
-  if (searchButton && menusItems.length > 0) {
-    // 获取 menusItems 的克隆节点
-    var menusItemsClone = menusItems[0].cloneNode(true);
+    if (searchButton && menusItems.length > 0) {
+      // 获取 menusItems 的克隆节点
+      var menusItemsClone = menusItems[0].cloneNode(true);
 
-    // 将克隆节点插入到 searchButton 之前
-    searchButton.parentNode.insertBefore(menusItemsClone, searchButton);
+      // 将克隆节点插入到 searchButton 之前
+      searchButton.parentNode.insertBefore(menusItemsClone, searchButton);
 
-    // 删除所有的 menusItems 元素
-    menusItems.forEach(function(item) {
-      item.parentNode.removeChild(item);
-    });
+      // 删除所有的 menusItems 元素
+      menusItems.forEach(function(item) {
+        item.parentNode.removeChild(item);
+      });
+    }
   }
 });
